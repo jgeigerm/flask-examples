186577208db813f34f6b4555e11f029ab3ae2f9b
jgeigerm <jmg2967@rit.edu>
2016-03-22 15:14:57 -0700

    added comments

--- skeleton/app/models.py
+++ skeleton/app/models.py
@@ -1,13 +1,21 @@
 from app import db
 from sqlalchemy.ext.hybrid import hybrid_property
-from flask.ext.security import UserMixin, RoleMixin
+from flask.ext.security import UserMixin, RoleMixin, current_user
 
+""" a third table must be created to relate users to roles by id.
+    this is an example of a many-to-many relationship in SQLAlchemy
+    i.e. a user can have many roles and roles can have many users
+"""
 roles_users = db.Table('roles_users',
         db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
         db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
+
+"""
+User: defines users and their attribles for the website
+Parents: db.Model, flask.ext.security.UserMixin
+"""
 class User(db.Model, UserMixin):
-    """Defines a user, all users have the same permission level"""
     id = db.Column(db.Integer(), primary_key=True)
     email = db.Column(db.String(255), index=True, unique=True)
     password = db.Column(db.String(255))
@@ -20,13 +28,30 @@ class User(db.Model, UserMixin):
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
 
+    """flask admin needs this to print the user correctly"""
     def __str__(self):
         return self.email
 
+    """
+    hybrid_property is cool. this is a lame example, but you can do complex
+    operations in the function and it can be called as an attribute
+    to tell if a user is an admin I would do user.is_admin instead of
+    user.is_admin() like a normal function
+    """
+    @hybrid_property
+    def is_admin(self):
+        return self.has_role('admin')
+
+
+"""
+Role: the table for roles on the site
+Parents: db.Model, flask.ext.security.RoleMixin
+"""
 class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
+    """flask admin needs this to print the role correctly"""
     def __str__(self):
         return self.name
